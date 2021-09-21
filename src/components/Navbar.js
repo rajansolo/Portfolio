165442fa19755d82b1154fddabc46a40774a32d7
Rajan Yadav <rj98458@gmail.com>
2021-09-21 13:38:13 +0545

    project setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <div className="container">
+  <a className="navbar-brand" href="#">RajanWD</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About me</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Services</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">How I work</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact me</a>
+      </li>
+      
+    </ul>
+    
+  </div>
+    </div>        
+</nav>
+    )
+}
+
+export default Navbar
